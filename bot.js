063d87e365da0d0a8b92bd9901b200836a46b92c
CursedRicardo <74558329+CursedRicardo@users.noreply.github.com>
2021-01-17 16:36:46 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,208 @@
+const Discord = require('discord.js');//
+const client = new Discord.Client();//
+const ayarlar = require('./ayarlar.json');//
+const chalk = require('chalk');//
+const moment = require('moment');//
+var Jimp = require('jimp');//
+const { Client, Util } = require('discord.js');//
+const fs = require('fs');//
+const db = require('quick.db');//
+const express = require('express');//
+require('./util/eventLoader.js')(client);//
+const path = require('path');//
+const snekfetch = require('snekfetch');//
+//
+
+var prefix = ayarlar.prefix;//
+//
+const log = message => {//
+    console.log(`${message}`);//
+};
+
+client.commands = new Discord.Collection();//
+client.aliases = new Discord.Collection();//
+fs.readdir('./komutlar/', (err, files) => {//
+    if (err) console.error(err);//
+    log(`${files.length} komut yüklenecek.`);//
+    files.forEach(f => {//
+        let props = require(`./komutlar/${f}`);//
+        log(`Yüklenen komut: ${props.help.name}.`);//
+        client.commands.set(props.help.name, props);//
+        props.conf.aliases.forEach(alias => {//
+            client.aliases.set(alias, props.help.name);//
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+client.on('warn', e => {
+    console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+client.on('error', e => {
+    console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(ayarlar.token);
+
+//-----------------------GİRENE-ROL-VERME----------------------\\     STG
+
+client.on("guildMemberAdd", member => {
+  member.roles.add('782181985509965854'); // UNREGİSTER ROLÜNÜN İDSİNİ GİRİN
+});
+
+//-----------------------GİRENE-ROL-VERME----------------------\\     STG
+
+
+
+//------------------------HOŞGELDİN-EMBEDLİ-----------------------\\     STG
+
+client.on("guildMemberAdd", member => {
+    require("moment-duration-format")
+      var üyesayısı = member.guild.members.cache.size.toString().replace(/ /g, "    ")
+      var üs = üyesayısı.match(/([0-9])/g)
+      üyesayısı = üyesayısı.replace(/([a-zA-Z])/g, "bilinmiyor").toLowerCase()
+      if(üs) {
+        üyesayısı = üyesayısı.replace(/([0-9])/g, d => {
+          return {
+            '0': `<:number0:800340331106402324>`,
+            '1': `<:number1:800340391432945704>`,
+            '2': `<:number2:800340479862243378>`,
+            '3': `<:number3:800340536929812490>`,
+            '4': `<:number4:800340582828474369>`,
+            '5': `<:number5:800340646111346708>`,
+            '6': `<:number6:800340766702436372>`,
+            '7': `<:number7:800340820989050910>`,
+            '8': `<:number8:800340894632771584>`,
+            '9': `<:number9:800340936780546050>`}[d];})}
+      const kanal = member.guild.channels.cache.find(r => r.id === "797925488185311273");
+      let register = '782959859297419296'
+    let user = client.users.cache.get(member.id);
+    require("moment-duration-format");
+      const kurulus = new Date().getTime() - user.createdAt.getTime();  
+     const gecen = moment.duration(kurulus).format(` YY **[Yıl,]** DD **[Gün,]** HH **[Saat,]** mm **[Dakika,]** ss **[Saniye]**`) 
+    var kontrol;
+  if (kurulus < 1296000000) kontrol = 'Hesap Durumu: Güvenilir Değil.'
+  if (kurulus > 1296000000) kontrol = 'Hesap Durumu: Güvenilir Gözüküyor.'
+    moment.locale("tr");
+  const embed = new Discord.MessageEmbed()
+  .setAuthor(member.guild.name, member.guild.iconURL({dynamic:true}))
+  .setDescription(`
+  <a:pikachu:784442572096667658> **<@`+member.id+`> Sunucumuza Katıldı ! **<a:pikachu:784442572096667658>
+  
+  <a:yildiz:784398441534062612>**Kayıt edilmek için <@&{register}> yetkililerine isim yaş veya teyit vermen yeterli !**<a:yildiz:784398441534062612>
+  
+  <a:yildiz_2:784449289056157746>  **Seninle birlikte `+üyesayısı+` kişiye ulaştık !** <a:yildiz_2:784449289056157746>
+  
+  <a:pin:784398992510812160>**Sunucumuzun kurallarına uymayı unutma!**<a:pin:784398992510812160>
+
+  <a:aaaaaa:785215298667282432>**Yetkililer gelene kadar lütfen sabret.**<a:aaaaaa:785215298667282432>
+
+  **İçeride keyifli vakitler geçirmeni dileriz.**`)
+  .setImage(`https://cdn.discordapp.com/attachments/717656449194393644/800344140608438272/standard.gif`)
+  kanal.send(embed)
+  kanal.send(`<@&${register}>`)
+});
+//------------------------HOŞGELDİN-EMBEDLİ-----------------------\\     STG
+
+
+//------------------------ŞÜPHELİ-HESAP-----------------------\\     STG
+
+client.on("guildMemberAdd", member => {
+    var moment = require("moment")
+    require("moment-duration-format")
+    moment.locale("tr")
+     var {Permissions} = require('discord.js');
+     var x = moment(member.user.createdAt).add(7, 'days').fromNow()
+     var user = member.user
+     x = x.replace("birkaç saniye önce", " ")
+     if(!x.includes("önce") || x.includes("sonra") ||x == " ") {
+    const kytsz = member.guild.roles.cache.find(r => r.id === "782181985509965854") 
+     var rol = member.guild.roles.cache.get("785072770127364116") // ŞÜPHELİ HESAP ROLÜNÜN İDSİNİ GİRİN
+     var kayıtsız = member.guild.roles.cache.get(kytsz) // UNREGİSTER ROLÜNÜN İDSİNİ GİRİN
+     member.roles.add(rol)
+     member.roles.remove(kytsz)
+
+  member.user.send('<a:WumpusSadCry:784475034898661396>**Selam Dostum Ne Yazık ki Sana Kötü Bir Haberim Var Hesabın 1 Hafta Gibi Kısa Bir Sürede Açıldığı İçin Fake Hesap Katagorisine Giriyorsun Lütfen Bir Yetkiliyle İletişime Geç Onlar Sana Yardımcı Olucaktır.**<a:WumpusSadCry:784475034898661396>')
+  setTimeout(() => {
+  
+  }, 1000)
+  
+  
+     }
+          else {
+  
+          }
+      });
+
+//------------------------ŞÜPHELİ-HESAP-----------------------\\     STG
---
+++ komutlar/stg-erkek.js
@@ -0,0 +1,87 @@
+const { MessageEmbed } = require('discord.js')
+const datab = require('quick.db')
+const ms = require('ms')
+const moment = require("moment");
+const { parseZone } = require("moment");
+
+exports.run =  async (client, message, args) => {
+  
+if(!['782959859297419296'].some(role => message.member.roles.cache.get(role)) && !message.member.hasPermission('ADMINISTRATOR')) 
+return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`${message.author} bu komutu kullanmak için yetkin bulunmamakta.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+  
+const tag = '🍵'   
+const erkekrol = message.guild.roles.cache.find(r => r.id === '774654724989386772') 
+const erkekrol2 = message.guild.roles.cache.find(r => r.id === '774654724989386772')
+const kayıtsız = message.guild.roles.cache.find(r => r.id === '782181985509965854')
+const genelchat = message.guild.channels.cache.find(c => c.id === '786858942583537674')
+const savelog = message.guild.channels.cache.find(c => c.id === '797925488185311273')
+
+if(!erkekrol) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`1. Erkek rolü ayarlanmamış/yanlış id girilmiş kayıt işlemine devam edilemiyor.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+if(!erkekrol2) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`2. Erkek rolü ayarlanmamış/yanlış id girilmiş kayıt işlemine devam edilemiyor.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+if(!kayıtsız) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`Kayıtsız rolü ayarlanmamış/yanlış id girilmiş kayıt işlemine devam edilemiyor.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+
+const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
+if(!member) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`${message.author} bir kullanıcı belirt.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+let name = args[1]
+let age = Number(args[2])
+if(!name) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`Bir isim belirtmelisin.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+if(!age) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`Bir yaş belirtmelisin.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+if(member.id === message.author.id) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`Kendini kayıt edemezsin.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+if(member.id === client.user.id) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`Bot kayıt edemezsin.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+if(member.id === message.guild.OwnerID) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`Sunucu sahibini kayıt edemezsin.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+if(member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`Belirtilen kullanıcı sizden üst/aynı pozisyonda işleme devam edilemiyor.`)
+.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+  
+  
+datab.add(`yetkili.${message.author.id}.kadin`, 1)
+datab.add(`yetkili.${message.author.id}.toplam`, 1)
+let alldata = datab.fetch(`yetkili.${message.author.id}.toplam`)
+
+member.setNickname(`${tag} ${name} | ${age}`)
+member.roles.add(erkekrol)
+member.roles.add(erkekrol2)
+member.roles.remove(kayıtsız)
+  
+genelchat.send(`<:hellodc:798858625509752865>**${member} Aramıza katıldı, hoşgeldin umarım keyifli vakit geçirirsin.**<:hellodc:798858625509752865> `)
+  
+savelog.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+.setDescription(`• Yetkili: ${message.author} | \`${message.author.id}\`\n• Kullanıcı: ${member} | \`${member.id}\`\n• Güncel İsim: \`${tag} ${name} | ${age}\`\n• Roller: ${erkekrol} \n• Kanal: <#${message.channel.id}> | \`${message.channel.id}\`\n• Kayıtlar: \`${alldata}\` `)
+.setColor('#65d8c4'))
+
+
+datab.push(`isim.${message.guild.id}`, {userID: member.id, isim: name, yas: age, role: erkekrol.id})}
+exports.conf = {enabled: true, guildOnly: true, aliases: ['erkek', 'e', 'boy', 'man', 'adam'], permLevel: 0}
+exports.help = {name: 'kayıt', description: "Etiketlenen kişiyi erkek rolleriyle kayıt eder.", usage: 'k!kayıt @etiket/id İsim Yaş'}
